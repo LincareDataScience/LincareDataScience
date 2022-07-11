93ad4c420284c2ff01ae0fd7b9c0510d17ed1280
JCeleste <jceleste@lincare.com>
2022-07-11 15:47:40 -0400

    Add new function Query_Linked_Server

---
+++ R/Query_Linked_Server.R
@@ -0,0 +1,23 @@
+Query_Linked_Server {
+  #' Query Linked SQLServer
+  #'
+  #' This function queries a Linked Server within SQL Server
+  #'
+  #' @param DSN The Data Source Name (EDW_IBMi, EDW_External)
+  #' @param query The Query Text
+  #' @param linked_server_name The Name of the Linked Server (EDWtoIBMi)
+  #' @return A Dataframe containing the Query Results
+  #' @examples Query <- "Select * from table"
+  #' Data <- Query_Linked_Server(DSN = "EDWtoIBMi",
+  #'                             query = Query,
+  #'                             linked_server_name = "EDWtoIBMi")
+  #' @export
+  Query_Linked_Server <- function(DSN, query, linked_server_name){
+    query <- gsub("'", "''", query)
+    query <- paste0("Exec ('", query, "') at [", linked_server_name, "]")
+    con <- RODBC::odbcConnect(DSN)
+    data <- RODBC::sqlQuery(con, query)
+    colnames(data) <- gsub(pattern = '\\"', "", colnames(data))
+    data
+  }
+}
