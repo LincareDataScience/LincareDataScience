5e2ca3fca93b8b62773875d7b24e448b98350aef
JCeleste <jceleste@lincare.com>
2022-07-11 18:22:50 -0400

    Add Location Hierarchy

---
+++ R/Location_Hierarchy.R
@@ -0,0 +1,19 @@
+#' Location Hierarchy
+#'
+#' Returns Lincare's Location Hierarchy
+#'
+#' @return A Dataframe
+#'
+#' @references DBI
+#'
+#' @examples Query <- "Select * from table"
+#' Data <- Query_Linked_Server(DSN = "EDWtoIBMi",
+#'                             query = Query,
+#'                             linked_server_name = "EDWtoIBMi")
+#'
+#' @export
+Location_Hierarchy <- function(){
+  con <- DBI::dbConnect(odbc::odbc(), "RStudio")
+  DBI::dbReadTable(conn = con, name = "Location_Hierarchy")
+}
+
